e5f0474aa4109eae4d97a13e08030654c91b028e
Ramon Gonzalez <ramon_gonzalez2@ucne.edu.do>
2025-06-03 18:46:58 -0400

    Agregando componentes necesarios

---
+++ Ramon_Gonzalez_AP1_P1/Models/Registro.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ramon_Gonzalez_AP1_P1.Models
+{
+    public class Registro
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
