86294965f925ebe0b0b9aab82498ac354d45d10c
Ramon Gonzalez <ramon_gonzalez2@ucne.edu.do>
2025-06-09 19:06:33 -0400

    Modificando nombre

---
+++ Ramon_Gonzalez_AP1_P1/Models/Aportes.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ramon_Gonzalez_AP1_P1.Models
+{
+    public class Aportes
+    {
+        [Key]
+        public int AportesId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public String Persona { get; set; } = String.Empty;
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "El Monto debe ser mayor a 0")]
+        public decimal Monto { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public DateOnly Fecha { get; set; } = DateOnly.FromDateTime(DateTime.Today);
+
+
+    }
+}
+
+
+//Listo para Iniciar
