af665954e1202d233597a32a77a4f791e1117789
Ramon Gonzalez <ramon_gonzalez2@ucne.edu.do>
2025-06-09 18:42:06 -0400

    Agregando Componentes necesarios

--- Ramon_Gonzalez_AP1_P1/DAL/Contexto.cs
+++ Ramon_Gonzalez_AP1_P1/DAL/Contexto.cs
@@ -1,6 +1,16 @@
-﻿namespace Ramon_Gonzalez_AP1_P1.DAL
+﻿using Microsoft.EntityFrameworkCore;
+using Ramon_Gonzalez_AP1_P1.Models;
+
+namespace Ramon_Gonzalez_AP1_P1.DAL
 {
-    public class Contexto
+    public class Contexto: DbContext 
     {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) 
+        {
+        }
+
+        public DbSet<Aportes> Aportes { get; set; }
+
+        
     }
 }
