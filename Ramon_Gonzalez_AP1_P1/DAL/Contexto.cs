ab8ea62671d5e6aab08b3e14c718c8c701e7f8c2
Ramon Gonzalez <ramon_gonzalez2@ucne.edu.do>
2025-06-09 18:46:23 -0400

    Modificando componente

--- Ramon_Gonzalez_AP1_P1/DAL/Contexto.cs
+++ Ramon_Gonzalez_AP1_P1/DAL/Contexto.cs
@@ -11,6 +11,23 @@ namespace Ramon_Gonzalez_AP1_P1.DAL
 
         public DbSet<Aportes> Aportes { get; set; }
 
-        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Aportes>()
+                .Property(a => a.Persona)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<Aportes>()
+                .Property(a => a.Persona)
+                .IsRequired()
+                .HasMaxLength(250);
+
+            modelBuilder.Entity<Aportes>()
+                .Property(a => a.Monto)
+                .HasPrecision(18, 2);
+        }
     }
 }
