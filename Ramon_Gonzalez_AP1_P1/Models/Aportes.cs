3d67c304b6fcdaab51f8308263d3c07f7e85fcf6
Ramon Gonzalez <ramon_gonzalez2@ucne.edu.do>
2025-06-13 16:58:22 -0400

    Agregando Validaciones necesarias

--- Ramon_Gonzalez_AP1_P1/Models/Aportes.cs
+++ Ramon_Gonzalez_AP1_P1/Models/Aportes.cs
@@ -9,6 +9,7 @@ namespace Ramon_Gonzalez_AP1_P1.Models
         public int AportesId { get; set; }
 
         [Required(ErrorMessage = "Este campo es obligatorio")]
+        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$", ErrorMessage ="El nombre no debe contener numeros ni caracteres especiales.")]
         public String Persona { get; set; } = String.Empty;
 
         [Range(0.01, double.MaxValue, ErrorMessage = "El Monto debe ser mayor a 0")]
